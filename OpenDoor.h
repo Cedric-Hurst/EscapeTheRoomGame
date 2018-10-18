e62a346f4468e625876599278caa9854da593435
Cedric-Hurst <churst283@bears.shawu.edu>
2018-10-18 16:02:23 -0400

    BuildingEscape class header file

---
+++ OpenDoor.h
@@ -0,0 +1,50 @@
+// Copyright Cedric Hurst 2017
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerVolume.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoor();
+	void CloseDoor();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+private:
+	UPROPERTY(EditAnywhere)
+	float OpenAngle;
+
+	UPROPERTY(EditAnywhere)
+	float CloseAngle;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	float DoorCloseDelay = 1.0f;
+
+	float LastDoorOpenTime;
+
+	AActor* ActorThatOpens;
+	AActor* Owner; // The owning door
+};
